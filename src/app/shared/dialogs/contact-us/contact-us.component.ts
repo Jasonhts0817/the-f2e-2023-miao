12f134a1cce7f4856cb12fae209c4f528c5cd6e4
Jason Huang <jason.hts0817@gmail.com>
2023-11-08 21:07:48 +0800

    feat: add contact us dialog

--- src/app/shared/dialogs/contact-us/contact-us.component.ts
+++ src/app/shared/dialogs/contact-us/contact-us.component.ts
@@ -2,16 +2,22 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DialogComponent } from '../dialog/dialog.component';
 import { Dialog, DialogRef } from '@angular/cdk/dialog';
+import { ButtonComponent } from '../../button.component';
+import { Theme } from 'src/app/core/enums/theme.enum';
+import { Size } from 'src/app/core/enums/size.enum';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [CommonModule, DialogComponent],
+  imports: [CommonModule, DialogComponent, ButtonComponent],
   templateUrl: './contact-us.component.html',
 })
 export class ContactUsComponent {
-  constructor(
-    public dialogRef: DialogRef,
-    private dialog: Dialog,
-  ) {}
+  Theme = Theme;
+  Size = Size;
+  isSuccess = false;
+  constructor(public dialogRef: DialogRef) {}
+  submit() {
+    this.isSuccess = true;
+  }
 }
