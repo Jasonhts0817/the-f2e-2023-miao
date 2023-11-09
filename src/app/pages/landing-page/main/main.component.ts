047ca9a970e7addd24e97b241f98b0b06f572571
Jason Huang <jason.hts0817@gmail.com>
2023-11-09 22:01:10 +0800

    feat: add mobile social media

--- src/app/pages/landing-page/main/main.component.ts
+++ src/app/pages/landing-page/main/main.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IconComponent } from 'src/app/shared/icon.component';
+import { Icon } from 'src/app/core/enums/icon.enum';
+import { Size } from 'src/app/core/enums/size.enum';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IconComponent],
   templateUrl: './main.component.html',
 })
-export class MainComponent {}
+export class MainComponent {
+  Icon = Icon;
+  Size = Size;
+}
