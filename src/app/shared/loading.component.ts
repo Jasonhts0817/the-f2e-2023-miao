27bdf3db450f6d6378a491647d1a0813811caedc
Jason Huang <jason.hts0817@gmail.com>
2023-11-09 12:37:48 +0800

    fix: loading page

--- src/app/shared/loading.component.ts
+++ src/app/shared/loading.component.ts
@@ -1,12 +1,4 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import {
-  NavigationCancel,
-  NavigationEnd,
-  NavigationError,
-  NavigationStart,
-  Router,
-  RouterEvent,
-} from '@angular/router';
 import * as lottie from 'lottie-web';
 @Component({
   selector: 'app-loading',
@@ -23,12 +15,6 @@ export class LoadingComponent implements AfterViewInit {
   @ViewChild('loading') loading!: ElementRef<HTMLDivElement>;
   showOverlay = true;
 
-  constructor(private router: Router) {
-    router.events.subscribe((event: any) => {
-      this.navigationInterceptor(event);
-    });
-  }
-
   ngAfterViewInit(): void {
     lottie.default.loadAnimation({
       container: this.loading.nativeElement, // the dom element that will contain the animation
@@ -37,25 +23,21 @@ export class LoadingComponent implements AfterViewInit {
       autoplay: true,
       path: 'assets/lottie/loading.json', // the path to the animation json
     });
+    this.showLoadingPage();
+    setTimeout(() => {
+      this.hideLoadingPage();
+    }, 1000);
   }
 
-  navigationInterceptor(event: RouterEvent): void {
-    switch (true) {
-      case event instanceof NavigationStart:
-        document.body.classList.add('overflow-hidden');
-        document.body.classList.add('h-[100dvh]');
-        this.showOverlay = true;
-        break;
-      case event instanceof NavigationEnd:
-      case event instanceof NavigationCancel:
-      case event instanceof NavigationError:
-        setTimeout(() => {
-          document.body.classList.remove('overflow-hidden');
-          document.body.classList.add('h-[100dvh]');
-          this.showOverlay = false;
-        }, 2000);
+  showLoadingPage() {
+    document.body.classList.add('overflow-hidden');
+    document.body.classList.add('h-[100dvh]');
+    this.showOverlay = true;
+  }
 
-        break;
-    }
+  hideLoadingPage() {
+    document.body.classList.remove('overflow-hidden');
+    document.body.classList.add('h-[100dvh]');
+    this.showOverlay = false;
   }
 }
